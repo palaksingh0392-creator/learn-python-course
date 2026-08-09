1c6c67fa4f32b3bee036bba910f69123571d6726
sujeet-93 <sujeet93@live.com>
2026-08-09 20:02:21 +0530

    Initial commit

---
+++ Add3Values.py
@@ -0,0 +1,18 @@
+#Add 3 Values in a program.
+#initialization and declaration
+num1 = int(input("enter the value of num1:"))
+num2 = int(input("enter the value of num2:"))
+num3 = int(input("enter the value of num3:"))
+
+#logic for addition
+
+sum = num1+num2+num3
+
+#print the values 
+print("the value of num1:",num1)
+print("the value of num2:",num2)
+print("the value of num3:",num3)
+
+#print sum of values
+ 
+print("the sum of all values:",sum)
