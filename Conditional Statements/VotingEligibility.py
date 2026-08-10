af617ed298ca759027447d3f16ff58b46d4721ee
Palak Singh <palak.singh0392@gmail.com>
2026-08-10 15:13:15 +0530

    Voting Eligibility
    Voting Eligibility
    Take age. If age is 18 or more, show Eligible to vote; otherwise show Not eligible.

---
+++ Conditional Statements/VotingEligibility.py
@@ -0,0 +1,8 @@
+# Voting Eligibility
+num1 = int(input("enter the value of num1 :"))
+
+#applied logic 
+if num1 >= 18 :
+    print(f"the person is eligible for voting")
+else :
+    print(f"the person is not eligible for voting")
