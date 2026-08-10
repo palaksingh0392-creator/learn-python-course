962ff516fb013b79d427068b9be0e19c005b01ab
Palak Singh <palak.singh0392@gmail.com>
2026-08-10 14:54:47 +0530

    Calculate Area of rectangle
    Take length and breadth. Calculate Area = length x bread

---
+++ variables/Rectanglearea.py
@@ -0,0 +1,9 @@
+# calculate area of rectangle 
+length = int(input("enter the value of length:"))
+breadth = int(input("enter the value of breadth:"))
+
+# rectangle of area logic
+area = length * breadth
+
+#print 
+print(f"the area of length {length} and breadth {breadth} is {area} ")
