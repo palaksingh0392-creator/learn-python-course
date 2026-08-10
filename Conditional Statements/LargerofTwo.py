6adf3f49328757d32530c7c080c6357931647eed
Palak Singh <palak.singh0392@gmail.com>
2026-08-10 15:10:35 +0530

    Larger of Two
     Larger of Two
    Take 2 numbers and display the larger one.

---
+++ Conditional Statements/LargerofTwo.py
@@ -0,0 +1,11 @@
+#Larger of Two
+num1 = int(input("enter the value of num1 :"))
+num2 = int(input("enter the value of num2 :"))
+
+#applied logic 
+if num1 > num2 :
+    print(f"the num1 is greator than num2")
+elif num1 < num2:
+    print(f"the num2 is greator than num1")
+else :
+    print(f"the num1 and num2 is equal")
