c69bcf5957f61743010756d41f76de5fb4ed6e7f
Palak Singh <palak.singh0392@gmail.com>
2026-08-10 15:07:19 +0530

    Add script to check if a number is positive, negative, or zero
    Take a number and tell whether it is positive, negative or zero

---
+++ Conditional Statements/Positive,NegativeorZero.py
@@ -0,0 +1,9 @@
+#Positive, Negative or Zero
+num = float (input("enter the value of num:"))
+
+if num > 0 :
+    print(f"The entered value {num} is positive")
+elif num < 0:
+    print(f"The entered value {num} is negative")
+else :
+    print(f"The entered value {num} is neutral")
