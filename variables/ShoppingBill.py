c7f45a99e2fe9998539f467c855ddee60419abf4
Palak Singh <palak.singh0392@gmail.com>
2026-08-10 14:55:59 +0530

    Shopping Bill
    Take the price of 3 products and calculate the total bill.

---
+++ variables/ShoppingBill.py
@@ -0,0 +1,10 @@
+# the shopping bill
+item1 = float(input("enter the value of item1:"))
+item2 = float(input("enter the value of item2:"))
+item3 = float(input("enter the value of item3:"))
+
+#calulate the total bill 
+sum = item1+item2+item3
+
+#print invoice
+print(f"item1 : {item1} , item2 : {item2} , item3 : {item3} and their sum is {sum}")
