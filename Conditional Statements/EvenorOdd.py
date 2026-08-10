77ac4817559d2aac7471e9928f477794090b842e
Palak Singh <palak.singh0392@gmail.com>
2026-08-10 15:09:34 +0530

    Even or Odd
    Take a number and tell whether it is even or odd.

---
+++ Conditional Statements/EvenorOdd.py
@@ -0,0 +1,8 @@
+#Even or Odd
+num = int (input("enter the value of num:"))
+
+#logic 
+if num%2 == 0 :
+    print("the entered value is even",num )
+else :
+    print("the entered value is odd",num )
