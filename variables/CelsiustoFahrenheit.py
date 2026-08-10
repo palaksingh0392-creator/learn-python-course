c0cc82812ae5955299cc4f5ae03bdb35712b99dc
Palak Singh <palak.singh0392@gmail.com>
2026-08-10 14:50:26 +0530

    convert Celsius to Fahrenheit
    Take temperature in Celsius and convert it using F = (C x 9/5) + 32

---
+++ variables/CelsiustoFahrenheit.py
@@ -0,0 +1,9 @@
+#convert celsus into fahrenheit
+cel = float(input("enter the value of celsius : "))
+
+# conversion logic
+
+fahrenheit = (cel*4.5)+32
+
+# print
+print(f"the given celsius is {cel} converted in fahranheit {fahrenheit}")
