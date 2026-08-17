ffb900d56e01c25e171f7cf0557e0e243bec89b2
Palak Singh <palak.singh0392@gmail.com>
2026-08-17 10:12:45 +0530

    Create a function that prints a greeting.
    Create a function that takes a person's name and prints a greeting.

---
+++ fuctions/greetfunct.py
@@ -0,0 +1,7 @@
+#Create a function that takes a person's name and prints a greeting
+
+name = input("enter your name, please :")
+def greet():
+    print(f"Hello, {name} in python series")
+
+greet()
