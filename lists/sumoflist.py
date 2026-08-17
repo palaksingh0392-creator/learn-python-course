8c497562c1b87e642344b70fc2727f2a3de4d0dc
Palak Singh <palak.singh0392@gmail.com>
2026-08-17 10:23:03 +0530

    sum of all numbers in a list without using sum().
    Find the sum of all numbers in a list without using sum().

---
+++ lists/sumoflist.py
@@ -0,0 +1,16 @@
+# 3. Find the sum of all numbers in a list without using sum()
+num1 = int(input("enter the value of num1 :"))
+num2 = int(input("enter the value of num2 :"))
+num3 = int(input("enter the value of num3 :"))
+num4 = int(input("enter the value of num4 :"))
+num5 = int(input("enter the value of num5 :"))
+
+num = [num1,num2,num3,num4,num5]
+print(num[0])
+print(num[1])
+print(num[2])
+print(num[3])
+print(num[4])
+
+num = [num1+num2+num3+num4+num5]
+print(num)
