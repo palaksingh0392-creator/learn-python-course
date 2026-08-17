3c50258aca68f2e4d81bf613045ddc5f589e696a
Palak Singh <palak.singh0392@gmail.com>
2026-08-17 10:24:56 +0530

    Take 5 no. from user, store and display the list
    Take 5 numbers from the user, store them in a list and display the list

---
+++ lists/userentrylist.py
@@ -0,0 +1,13 @@
+# 2. Take 5 numbers from the user, store them in a list and display the list.
+num1 = int(input("enter the value of num1 :"))
+num2 = int(input("enter the value of num2 :"))
+num3 = int(input("enter the value of num3 :"))
+num4 = int(input("enter the value of num4 :"))
+num5 = int(input("enter the value of num5 :"))
+
+num = [num1,num2,num3,num4,num5]
+print(num[0])
+print(num[1])
+print(num[2])
+print(num[3])
+print(num[4])
