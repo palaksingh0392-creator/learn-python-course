65309bc273a5a85e123aeb8401a572a79f8ab794
Palak Singh <palak.singh0392@gmail.com>
2026-08-17 10:18:03 +0530

    5 student names and print every name.
    Create a list containing 5 student names and print every name.

---
+++ lists/5stdname.py
@@ -0,0 +1,8 @@
+#  Create a list containing 5 student names and print every name.
+
+std = ["Palak","Kanak","Disha","Manasvi","Ravi"]
+print (std[0])
+print (std[1])
+print (std[2])
+print (std[3])
+print (std[4])
