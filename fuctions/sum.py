5c4216afdeeba1851edfd0cbb3b6fb5a07151990
Palak Singh <palak.singh0392@gmail.com>
2026-08-17 10:15:04 +0530

    takes two numbers and returns their sum
    Create a function that takes two numbers and returns their sum

---
+++ fuctions/sum.py
@@ -0,0 +1,12 @@
+#Create a function that takes two numbers and returns their sum
+num1 = int(input("enter the value of num1 :"))
+num2 = int(input("enter the value of num2 :"))
+
+#logic
+def sum(num1,num2): 
+  
+    return  num1+num2
+    
+sum(num1,num2)
+add = num1+num2
+print(f"addition of {num1} and {num2} is {add}")
